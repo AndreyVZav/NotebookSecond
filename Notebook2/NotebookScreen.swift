e5767fc3f72f35fc0fb527dc72d9eb3cf499a3d9
Kirill Shakhansky <fuzzzzyboy@Kirills-Air.home>
2024-06-22 22:37:41 +0500

    set alignment

--- Notebook2/NotebookScreen.swift
+++ Notebook2/NotebookScreen.swift
@@ -49,7 +49,7 @@ struct NotebookScreen: View {
         
         
         ScrollView {
-            VStack {
+            VStack(alignment: .leading) {
                 // Note2 - это Identifiable, на это никак не используется. Либо Identifiable, либо, что мне больше нравится использовать ForEach(notes2) {...} и в остальных местах вместо selectedNoteIndex использовать selectedNoteID// поправил
                 ForEach(notesManager.notes) { note in
                     Text(note.text)
